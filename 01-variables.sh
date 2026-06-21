c902e105da4099f160fd2e2e84b4ffd765e0fb87
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 08:29:53 +0530

    shell script started

---
+++ 01-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "hello world.sh"
+
+echo "Trump:hey Iran, we are going to blast you"
+echo "Iran: hey Trump, you are welcome"
+echo "Trump: hey iran, im serious"
+echo "Iran: hey Trump, we are also serious"
