7f62c154fdeb12f8ab26e69b20ab4e9cce0f69e2
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 08:35:39 +0530

    shell script started

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hey $person2, we are going to blast you"
+echo "$person2: hey $person1, you are welcome"
+echo "$person1: hey $person2, im serious"
+echo "$person2: hey $person1, we are also serious"
