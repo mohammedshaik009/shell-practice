0ba15a137f998e3ceec42586b31cee5d6bf1108d
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 09:21:56 +0530

    shell script passed

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 ARRAY_NAMES=("value1" "value2" "value3")
-echo "arra_names are :${ARRAY_NAMES[@]}"
+echo "array_names are :${ARRAY_NAMES[@]}"
+echo "array_names are :${ARRAY_NAMES[0]}"
+echo "array_names are :${ARRAY_NAMES[1]}"
+echo "array_names are :${ARRAY_NAMES[2]}"
+
+MOVIES=("rrr" "varanasi" "pushpa")
+echo "movies are:${MOVIES[@]}"
+echo "movies are:${MOVIES[0]}"
+echo "movies are:${MOVIES[1]}"
+echo "movies are:${MOVIES[2]}"
