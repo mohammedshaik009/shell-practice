0589b443106744a6852da716f135f7585bcf2665
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 09:26:52 +0530

    shell script passed

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -11,3 +11,20 @@ echo "movies are:${MOVIES[@]}"
 echo "movies are:${MOVIES[0]}"
 echo "movies are:${MOVIES[1]}"
 echo "movies are:${MOVIES[2]}"
+
+#######
+echo "all variables passed to this script:$@"
+echo "number of variables passed:$#"
+echo "first variable:$1"
+echo "name of the script:$0"
+echo "PID of script:$$"
+echo "user of the script:$USER"
+echo "pwd of the script:$PWD"
+echo "home directory of the script:$HOME"
+sleep 5 &
+echo "PID of the running first command in the script:$!"
+wait 5 
+echo "random number:$RANDOM"
+echo "line number of the script:$LINENO"
+echo "script executed in seconds:$SEONDS"
+echo "exit code of the previous command:$?"
