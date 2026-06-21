ce3c8396ef63d36b0a92f182cf43244b38cf9b0c
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 09:36:20 +0530

    shell script passed

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -12,26 +12,3 @@ echo "movies are:${MOVIES[0]}"
 echo "movies are:${MOVIES[1]}"
 echo "movies are:${MOVIES[2]}"
 
-#######
-echo "all variables passed to this script:$@"
-echo "number of variables passed:$#"
-echo "first variable:$1"
-echo "name of the script:$0"
-echo "PID of script:$$"
-echo "user of the script:$USER"
-echo "pwd of the script:$PWD"
-echo "home directory of the script:$HOME"
-sleep 5 &
-echo "PID of the running first command in the script:$!"
-wait 5 
-echo "random number:$RANDOM"
-echo "line number of the script:$LINENO"
-echo "script executed in seconds:$SECONDS"
-ijefhuwehfuewhf
-echo "exit code of the previous command:$?"
-
-MAHI=("kavya" "sweety" "kalpana")
-my girls are:${MAHI[@]}"
-my girls are:${MAHI[0]}"
-my girls are:${MAHI[1]}"
-my girls are:${MAHI[2]}"
