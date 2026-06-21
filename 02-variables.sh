7d8d6ea5c701d099a36e02f9f4e8066156901595
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 08:34:06 +0530

    shell script started

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=Trump
+person2=Iran
+
+echo "$person1: hey $person2, we are going to blast you"
+echo "$person2: hey $person1, you are welcome"
+echo "$person1: hey $person2, im serious"
+echo "$person2: hey $person1, we are also serious"
