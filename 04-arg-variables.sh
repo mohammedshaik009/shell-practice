0cc70c3ac3b949479eef8b2a9aede3178ea3427c
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-22 16:42:46 +0530

    shell script started

---
+++ 04-arg-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hey $person2, we are going to blast you"
+echo "$person2: hey $person1, you are welcome"
+echo "$person1: hey $person2, we are serious"
+echo "$person2: hey $person1, we are aslo serious"
