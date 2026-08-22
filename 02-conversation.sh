608480751edc0e816bdfc924fa550f21dfa0a403
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-22 16:27:33 +0530

    shell script started

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: hey Iran, we are going to blast you"
+echo "Iran: hey Trump, you are welcome"
+echo "Trump: hey Iran, we are serious"
+echo "Iran: hey Trump, we are aslo serious"
