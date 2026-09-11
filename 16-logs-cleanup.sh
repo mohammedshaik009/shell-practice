ec0118f226f211ed8f285f81edbe6e5d0d972591
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-11 18:51:59 +0530

    shell script started

--- 16-logs-cleanup.sh
+++ 16-logs-cleanup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SOURCE_DIR=$2
+SOURCE_DIR=$1
 DAYS=${2:-14}  #default to 14 days
 
 if [ -z $SOUCE_DIR ]; then
     echo "ERROR:: Misssing parameters"
-    echo "USAGE: $0 <source_dir> [days(optinal default to 14 days)]"
+    echo "USAGE:: $0 <source_dir> [days(optinal default to 14)]"
 fi
 
+if [ ! -d $SOUCRE_DIR ]; then
+    echo "ERROR: $SOURCE_DIR does not exist"
+    exit 1
+fi
