191691853940631271254ffbc1f5c27d30d15efe
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-11 20:29:12 +0530

    shell script started

--- 16-logs-cleanup.sh
+++ 16-logs-cleanup.sh
@@ -8,3 +8,8 @@ if [ -z $SOURCE_DIR ]; then
     echo "USAGE:: $0 <source_dir> [days(optinal default to 14)]"
     exit 1
 fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "ERROR: directory: $SOURCE_DIR does not exist"
+    exit 1
+fi
