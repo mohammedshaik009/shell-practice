4b48d079838838f7439533c6f22b934cd336af92
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-14 17:27:05 +0530

    shell script started

--- 17-logs-archieve.sh
+++ 17-logs-archieve.sh
@@ -4,39 +4,44 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #default to 14 days
 
-if [ -z $SOURCE_DIR ] || [ -z $DEST_DIR ]; then
-    echo "ERROR:: Either source_dir or dest_dir Missing"
-    echo "USAGE:: $0 <source_dir> <dest_dir> [14 (optional deafault to days)]"
+if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
+    echo "ERROR:: Either source_dir or Dest_dir Missing"
+    echo "USAGE:: $0 <source_dir> <dest_dir> (days[default to 14])"
     exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d "$SOURCE_DIR" ]; then
     echo "directory $SOURCE_DIR does not exist"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ]; then
+if [ ! -d "$DEST_DIR" ]; then
     echo "directory $DEST_DIR does not exist"
     exit 1
 fi
 
-echo "scanning for $SOURCE_DIR and $DEST_DIR for log files older than 14days"
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+echo "scanning for $SOURCE_DIR log files older than $DAYS"
+FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +$DAYS)
 
 if [ -z "$FILES" ]; then
-    echo "no log files older than 14 days found"
-    exit 0
+    echo "no log files found older than 14 days"
+    exit 0 
 fi
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-ARCHEIVE_FILE="$DEST_DIR/logs-archieve-$TIMESTAMP.tar.gz"
+
+TIMESTAMP=$(date "+%Y-%m-%d %H-%M-%S")
+ARCHEIVE_FILE="$DEST_DIR/logs-archieve-$TIMESTAMP tar.gz"
 
 tar -czvf "$ARCHEIVE_FILE" $FILES
 
 if [ $? -eq 0 ]; then
-echo "Archeival is success, deleting the files"
+    echo "archeival is success deleting the files"
     while IFS= read -r FILE
-    do
-    rm -f $FILE
-    echo "Delete file $FILE"
-done <<< "$FILES"
+        do
+            echo "File is $FILE"
+            rm -f "$FILE"
+            echo "Delete File $FILE"
+    done <<< "$FILES"
+else
+    echo "ERROR:archieval is FAILED"
+    exit 1
 fi
---
+++ 18-disk-usage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -HT | grep -v Filesystem)
+
+while IFS= read -r line
+do
+
+done <<< "$DISK_USAGE"
