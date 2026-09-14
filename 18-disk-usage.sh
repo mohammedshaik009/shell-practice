054efaf89aaa4b7b5f47bba070fa3b3add886cd2
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-14 17:49:25 +0530

    shell script started

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -5,8 +5,8 @@ USAGE_THRESHOLD=10
 
 while IFS= read -r line
 do
-    echo USAGE=$( echo $line | awk -f '{print $6}' | cut -d "%" -f1 )
-    PARTITION=$(echo $line | awk -f '{print $7}')
+    echo USAGE=$( echo "$line" | awk '{print $6}' | cut -d "%" -f1 )
+    PARTITION=$(echo "$line" | awk '{print $7}')
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE="High disk usage on $PARTITION: $USAGE"
     fi    
