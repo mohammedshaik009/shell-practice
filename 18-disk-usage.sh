b4bdbfa95b65c7374f585317d9218218a3c718cf
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-09-14 17:54:34 +0530

    shell script started

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -8,7 +8,7 @@ do
     USAGE=$( echo "$line" | awk '{print $6}' | cut -d "%" -f1 )
     PARTITION=$(echo "$line" | awk '{print $7}')
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE="High disk usage on $PARTITION: $USAGE"
+        MESSAGE+="High disk usage on $PARTITION: $USAGE"
     fi    
 done <<< "$DISK_USAGE"
 
